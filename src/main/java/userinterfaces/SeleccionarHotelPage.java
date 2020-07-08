6335a5ac1ae189a0a9b7c23b562b4c582d413225
branye <branye82@gmail.com>
2020-07-08 01:51:14 -0500

    validacion precios

--- src/main/java/userinterfaces/SeleccionarHotelPage.java
+++ src/main/java/userinterfaces/SeleccionarHotelPage.java
@@ -25,4 +25,19 @@ public class SeleccionarHotelPage {
     public static final Target BTN_SEARCH =
             Target.the("campo - Numero de niños").located(By.xpath("//form[@name=\"HOTELS\"]//button[contains(text(),'Search')]"));
 
+    public static final Target TXT_PRICE_TAG =
+            Target.the("campo - Precios en pantalla").located(By.xpath("//*/span[@class='text-secondary']"));
+
+    public static final Target LOWER_PRICE_TAG =
+            Target.the("campo - Precios mas bajo").locatedBy("(//*/span[@class='text-secondary'])[text()='{0}']");
+
+    public static final Target LOWER_PRICE_HOTEL_TITTLE =
+            Target.the("campo - Precios mas bajo").locatedBy("((//*/span[@class='text-secondary'])[text()='{0}']//ancestor::div[@class='col-12 col-auto o1']/div[1])/div[3]/div/div[2]");
+
+    public static final Target COOKIES_BUTTON =
+            Target.the("campo - boton aceptar cookies").locatedBy("//*/button[@class='cc-btn cc-dismiss']");
+
+    public static final Target TXT_SPA_CHARACTERISTIC =
+            Target.the("campo - texto contiene spa").locatedBy("(//*/span[@class='amint-text'])[text()='SPA']");
+
 }
--- src/test/java/com/hotel/stepdefinitions/ReservaHotelSteps.java
+++ src/test/java/com/hotel/stepdefinitions/ReservaHotelSteps.java
@@ -4,16 +4,25 @@ import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import exceptions.TravelExceptions;
+import net.serenitybdd.screenplay.GivenWhenThen;
 import net.serenitybdd.screenplay.actors.OnlineCast;
+import org.hamcrest.core.IsEqual;
+import questions.CheckIfHotelIncludes;
 import tasks.GoTravelsPage;
 import tasks.OpenPage;
 import tasks.Reservar;
 import io.cucumber.datatable.DataTable;
+import tasks.SelectHotel;
 
 
 import java.util.List;
+import java.util.Map;
 
+import static exceptions.TravelExceptions.SELECTED_HOTEL_DOES_NOT_CONTAINS;
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 import static net.serenitybdd.screenplay.actors.OnStage.*;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 public class ReservaHotelSteps {
     @Before
@@ -36,14 +45,15 @@ public class ReservaHotelSteps {
 
     @When("selecciona el hospedaje con el precio mas bajo")
     public void seleccionaElHospedajeConElPrecioMasBajo() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        theActorInTheSpotlight().attemptsTo(SelectHotel.byLowerPrice());
     }
 
-    @Then("se muestra el detalle de la reserva")
-    public void seMuestraElDetalleDeLaReserva() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+    @Then("verificar que el detalle de la reserva contenga la siguiente informacion")
+    public void verificarQueElDetalleDeLaReservaContengaLaSiguienteInformacion(DataTable table) {
+        List<List<String>> hotel = table.asLists(String.class);
+        theActorInTheSpotlight().should(seeThat("Contenga en sus detalles SPA",
+                CheckIfHotelIncludes.spa(),
+                equalTo(hotel.get(1).get(0))).orComplainWith(TravelExceptions.class, SELECTED_HOTEL_DOES_NOT_CONTAINS));
     }
 
 }
